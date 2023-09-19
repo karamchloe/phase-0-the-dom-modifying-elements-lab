097b06da8ae4af4cd7ee88c6bb2d50a136610f42
Chloe Park <kpark0119@gmail.com>
2023-09-18 20:03:03 -0400

    updates html through index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.querySelector("main");
+
+main.remove();
+//#main= grabbing the id by main
+//querySelector main --> selecting the <main> tag
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+
+newHeader.innerHTML = "Karam Park is the champion";
+
+
